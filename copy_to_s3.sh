7ff1d561c315b6e31e02aa40e848b8d3d5f9cbf0
root <nwanavit@gmail.com>
2019-12-27 02:41:05 +0000

    add scripts to copy to s3

--- copy_to_s3.sh
+++ copy_to_s3.sh
@@ -1,1 +1,4 @@
-aws2 s3 cp ./results s3://nicrl/results/p3/ --recursive
+current_date=`date +"%Y-%m-%d-%T"`
+echo $current_date
+aws2 s3 cp ./results s3://nicrl/results/p3/$current_date/ --recursive
+aws2 s3 cp ./models s3://nicrl/results/p3/$current_date/model/ --recursive
